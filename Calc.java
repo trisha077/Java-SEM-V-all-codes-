ed46f06ca87e5a15fef6a50ccd94bcee08c999ec
trisha077 <trishatrivedi3010@gmail.com>
2026-08-12 00:12:04 +0530

    Implement basic calculator functionality in Calc.java

---
+++ Calc.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Calc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print("Enter +, -, *, /: ");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+': System.out.println(a + b); break;
+            case '-': System.out.println(a - b); break;
+            case '*': System.out.println(a * b); break;
+            case '/': System.out.println(a / b); break;
+            default: System.out.println("Invalid");
+        }
+    }
+}
