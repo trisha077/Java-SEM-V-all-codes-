2e56c96f870744c69c681b25cb8f80096a8ea0c9
trisha077 <trishatrivedi3010@gmail.com>
2026-08-12 00:25:11 +0530

    Add String class to check length parity

---
+++ string.java
@@ -0,0 +1,10 @@
+public class String {
+    public static void main(String[] args) {
+        String s = "Hello";
+
+        if (s.length() % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
