3a3c01d547411a0f549b4c05b1956876a37de557
trisha077 <trishatrivedi3010@gmail.com>
2026-08-12 00:22:40 +0530

    Add Wrap2 class to sum two string numbers

---
+++ wrap2.java
@@ -0,0 +1,10 @@
+public class Wrap2 {
+    public static void main(String[] args) {
+        String a = "20", b = "30";
+
+        Integer x = Integer.valueOf(a);
+        Integer y = Integer.valueOf(b);
+
+        System.out.println("Sum = " + (x + y));
+    }
+}
